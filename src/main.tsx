31cba5389fc78dc98ad8682ea6bff12f49b60503
Thomas <thomas.e.hime@gmail.com>
2025-07-22 23:07:30 +0100

    hashrouter

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/index.scss";
 import { HelmetProvider } from "react-helmet-async";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import { ErrorBoundary } from "./components/error-boundary/ErrorBoundary.tsx";
 import { RootFallback } from "./components/error-boundary/RootFallback.tsx";
 import { routes } from "./routing/routes.tsx";
@@ -13,7 +13,7 @@ createRoot(document.getElementById("root")!).render(
     <ErrorBoundary inCaseOfError={<RootFallback />}>
       <ThemeContextProvider>
         <HelmetProvider>
-          <RouterProvider router={createBrowserRouter(routes)} />
+          <RouterProvider router={createHashRouter(routes)} />
         </HelmetProvider>
       </ThemeContextProvider>
     </ErrorBoundary>
